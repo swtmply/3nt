6c3ac86b6d4799dd6b6860a5cb00b308f8fc3ad3
swtmply <delosreyesjohnallen@gmail.com>
2025-04-18 14:31:56 +0800

    🎨 styles: updated tab bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,48 @@
 import { Stack, Tabs } from "expo-router";
+import { Clock, Wallet } from "lucide-react-native";
 
 import "./styles/global.css";
 
 export default function RootLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="history" options={{ title: "History" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          height: 100,
+          paddingVertical: 0,
+        },
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+        tabBarItemStyle: {
+          height: "100%",
+          padding: 0,
+          margin: 0,
+        },
+        tabBarIconStyle: {
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: 0,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: (props) => <Wallet {...props} />,
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: (props) => <Clock {...props} />,
+        }}
+      />
     </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,6 @@
-import { verifyInstallation } from "nativewind";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  verifyInstallation();
-
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Home</Text>
